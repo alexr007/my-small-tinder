6118d01901f34b212c313d9d197f9e374647f313
yamnyk <yamnyk.vlog@gmail.com>
2018-06-14 00:34:49 +0300

    writing on browser by Servlet

---
+++ mysmalltinder/src/main/java/utils/LocalTinderServer.java
@@ -0,0 +1,21 @@
+package utils;
+
+import controllers.UsersServlet;
+import org.eclipse.jetty.server.Server;
+import org.eclipse.jetty.servlet.ServletContextHandler;
+import org.eclipse.jetty.servlet.ServletHolder;
+
+public class LocalTinderServer {
+	public static void main(String[]args) throws Exception{
+		//creating new instance of class Server from jetty lib
+		//using anonymous class for not initialise any variables
+		new Server(8080){{
+			setHandler(new ServletContextHandler(){{
+				//create endpoint for servlet without any variables
+				addServlet(new ServletHolder(new UsersServlet()), "/users/*");
+			}});
+			start();
+			join();
+		}};
+	}
+}
