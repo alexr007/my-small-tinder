53aa7e06a695cf020494c6275cd8f7a0b5c95768
yamnyk <yamnyk.vlog@gmail.com>
2018-06-14 02:14:53 +0300

    add page users.html and show it in browser | WORKS

--- mysmalltinder/src/main/java/controllers/UsersServlet.java
+++ mysmalltinder/src/main/java/controllers/UsersServlet.java
@@ -1,15 +1,30 @@
 package controllers;
 
+import freemarker.template.Configuration;
+import freemarker.template.Template;
+import freemarker.template.TemplateExceptionHandler;
+
 import javax.servlet.ServletException;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+import java.io.File;
 import java.io.IOException;
+import java.io.Writer;
 
 public class UsersServlet extends HttpServlet {
 	@Override
 	protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
-		resp.getWriter().print("<h1>Congrats! This tiny little peace of code works.</h1>");
+		Configuration cfg = new Configuration(Configuration.VERSION_2_3_28);
+		String appDir = System.getProperty("user.dir");
+		cfg.setDirectoryForTemplateLoading(new File(appDir + "/mysmalltinder/lib/html"));
+		cfg.setDefaultEncoding("UTF-8");
+		cfg.setTemplateExceptionHandler(TemplateExceptionHandler.RETHROW_HANDLER);
+		cfg.setLogTemplateExceptions(false);
+		cfg.setWrapUncheckedExceptions(true);
+		
+		Template tmpl = cfg.getTemplate("users.html");
+		resp.getWriter().write(String.valueOf(tmpl));
 	}
 	
 	@Override
