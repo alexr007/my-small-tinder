d9d5e100693bff78def1d4b5a88588da1e24ebd2
yamnyk <notAunicorN2018>
2018-07-14 11:13:51 +0300

    add uuid IDGenerator, but it's not finished

--- src/main/java/ua/danit/utils/GeneratorID.java
+++ src/main/java/ua/danit/utils/GeneratorID.java
@@ -6,18 +6,7 @@ public class GeneratorID {
     private int length;
 
     public static Integer generateNewID(){ //toDo: check Native Java implementation (UUID?)
-//        UUID.randomUUID().
-
-        Integer source = (int)System.currentTimeMillis();
-        String stringId = "";
-        for (int i = 0; i < 6; i++) {
-            source = source << 1;
-        }
-        for (int i = 0; i < 9; i++) {
-            stringId += Math.abs(source % 10);
-            source = (source / 10)*9;
-        }
-        Integer id = Integer.parseInt(stringId);
+        Integer id = Integer.parseInt(UUID.randomUUID().toString());
         return id;
     }
 
